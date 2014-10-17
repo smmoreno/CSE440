012150268a6c2b60230d7a4fc15eda4d32cb401d
smmoreno <samuel_moreno@sbcglobal.net>
2014-10-16 18:24:42 -0700

    Completed Zombie Conga Tutorial

---
+++ ZombieConga/Assets/Scripts/ZombieAnimator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAnimator : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public float framePerSecond;
+
+	private SpriteRenderer spriteRenderer;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		spriteRenderer = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int index = (int)(Time.timeSinceLevelLoad * framePerSecond);
+		index = index % sprites.Length;
+		spriteRenderer.sprite = sprites [index];
+	}
+}
---
+++ ZombieConga/Assets/Sprites/CatUpdater.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CatUpdater : MonoBehaviour {
+	
+	private CatController catController;
+
+
+	void OnBecameInvisible() {
+		catController.OnBecameInvisible();
+	}
+
+	// Use this for initialization
+	void Start () {
+		catController = transform.parent.GetComponent<CatController>();  
+	}
+
+	void GrantCatTheSweetReleaseOfDeath()
+	{
+		Destroy( transform.parent.gameObject );
+	}
+
+
+	void UpdateTargetPosition()
+	{
+		catController.UpdateTargetPosition();
+	}
+}
