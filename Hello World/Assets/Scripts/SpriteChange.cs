1aefed26458e62c04d119a9aa3ca7937560ec19d
smmoreno <samuel_moreno@sbcglobal.net>
2014-10-23 20:50:17 -0700

    Hello World
    A program that involves the moving and changing of 2D sprites.

---
+++ Hello World/Assets/Scripts/SpriteChange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteChange : MonoBehaviour {
+
+	public Sprite sprite1;
+	public Sprite sprite2;
+	//drag sprites
+	private SpriteRenderer sr;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			if (sr.sprite == sprite1)
+				sr.sprite = sprite2;
+		else
+			sr.sprite = sprite1;
+	}
+}
---
+++ Hello World/Assets/Scripts/SpriteMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteMove : MonoBehaviour {
+
+	public float moveSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 tempPosition = transform.position;
+		if (Input.GetAxis ("Horizontal") > 0) {
+			tempPosition.x += moveSpeed;
+			//transform.position += 0.5f;
+		}
+		if (Input.GetAxis ("Horizontal") < 0) {
+			tempPosition.x -= moveSpeed;
+		}
+		if (Input.GetAxis ("Vertical") > 0) {
+			tempPosition.y += moveSpeed;
+		}
+		if (Input.GetAxis ("Vertical") < 0) {
+			tempPosition.y -= moveSpeed;
+		}
+		
+		transform.position = tempPosition;
+	}
+}
